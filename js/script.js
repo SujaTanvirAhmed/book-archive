cb714a04a9fe8fbc1cf26cfa498446e8b4ff8e53
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2021-09-03 17:39:00 +0600

    refined some codes and added notification code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const message = document.getElementById("message");
+let msg = "";
 const loader = document.getElementById("loader");
 const searchResult = document.getElementById("search-result");
 const searchButton = document.getElementById("search-btn");
@@ -11,6 +13,23 @@ const toggleLoader = status => {
         loader.style.display = "block";
     } else if (status === "off") {
         loader.style.display = "none";
+    }
+};
+
+const toggleNotification = status => {
+    if (status === "on") {
+        message.style.opacity = "1";
+    } else if (status === "off") {
+        message.style.opacity = "0";
+    }
+};
+
+const setNotification = (msg, status) => {
+    message.innerText = "==> " + msg;
+    if (status === "success") {
+        message.style.color = "#333";
+    } else if (status === "fail") {
+        message.style.color = "red";
     }
 };
 
@@ -29,80 +48,99 @@ const getCoverPhoto = (coverId) => {
         .catch(err => console.log(err));
 };
 
+const handleError = err => {
+    console.log(err);
+    msg = "Search failed! Please check net connection!!";
+    setNotification(msg, "fail");
+    toggleNotification("on");
+    toggleLoader("off");
+};
+
+const handleData = data => {
+    if (data.numFound === 0) {
+        // Notification message
+        msg = "No books found with this name!";
+        setNotification(msg, "fail");
+        toggleNotification("on");
+    } else if (data.numFound > 0) {
+        // Notification message
+        msg = `Showing ${data.docs.length} of ${data.numFound} entries!`;
+        setNotification(msg, "success");
+        toggleNotification("on");
+
+        // Book serial counter
+        let srlCounter = 0;
+
+        // Loop through each book
+        data.docs.forEach(book => {
+            srlCounter++;
+            const trEl = document.createElement("tr");
+
+            // Book title
+            if (!book.title) {
+                trEl.dataset.title = "empty";
+            } else {
+                trEl.dataset.title = book.title;
+            }
+
+            // Author name
+            if (!book.author_name) {
+                trEl.dataset.author_name = "empty";
+            } else {
+                trEl.dataset.author_name = getNames(book.author_name);
+            }
+
+            // Publisher
+            if (!book.publisher) {
+                trEl.dataset.publisher = "empty";
+            } else {
+                trEl.dataset.publisher = getNames(book.publisher);
+            }
+
+            // ISBN
+            if (!book.isbn) {
+                trEl.dataset.isbn = "empty";
+            } else {
+                trEl.dataset.isbn = book.isbn[0];
+            }
+
+            // Cover photo
+            if (!book.cover_i) {
+                trEl.dataset.cover_i = "empty";
+            } else {
+                trEl.dataset.cover_i = book.cover_i;
+            }
+
+            // Create row of result table
+            const trElHtml =
+                `<td>${srlCounter}</td>
+            <td>${book.title}</td>
+            <td>${book.author_name}</td>
+            <td>${book.publisher}</td>
+            <td>${book.first_publish_year}</td>`;
+            trEl.innerHTML = trElHtml;
+            bookTableBody.appendChild(trEl);
+        });
+
+        searchResult.style.display = "block";
+    }
+
+    toggleLoader("off");
+};
+
+// data fetcher
 const getBooks = (searchText) => {
     const bookUrl = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(bookUrl)
         .then(response => response.json())
         .then(data => handleData(data))
-        .catch(err => console.log(err));
-};
-
-const handleData = data => {
-    // Notification message
-    let msg = "Search failed! Connection error or invalid search-string!!";
-    // msg = `Showing ${part} of ${whole} entries`;
-
-    // Book serial counter
-    let srlCounter = 0;
-
-    // Loop through each book
-    data.docs.forEach(book => {
-        srlCounter++;
-        const trEl = document.createElement("tr");
-
-        // Book title
-        if (!book.title) {
-            trEl.dataset.title = "empty";
-        } else {
-            trEl.dataset.title = book.title;
-        }
-
-        // Author name
-        if (!book.author_name) {
-            trEl.dataset.author_name = "empty";
-        } else {
-            trEl.dataset.author_name = getNames(book.author_name);
-        }
-
-        // Publisher
-        if (!book.publisher) {
-            trEl.dataset.publisher = "empty";
-        } else {
-            trEl.dataset.publisher = getNames(book.publisher);
-        }
-
-        // ISBN
-        if (!book.isbn) {
-            trEl.dataset.isbn = "empty";
-        } else {
-            trEl.dataset.isbn = book.isbn[0];
-        }
-
-        // Cover photo
-        if (!book.cover_i) {
-            trEl.dataset.cover_i = "empty";
-        } else {
-            trEl.dataset.cover_i = book.cover_i;
-        }
-
-        // Create row of result table
-        const trElHtml =
-            `<td>${srlCounter}</td>
-            <td>${book.title}</td>
-            <td>${book.author_name}</td>
-            <td>${book.publisher}</td>
-            <td>${book.first_publish_year}</td>`;
-        trEl.innerHTML = trElHtml;
-        bookTableBody.appendChild(trEl);
-    });
-
-    toggleLoader("off");
-    searchResult.style.display = "block";
+        .catch(err => handleError(err));
 };
 
 // handle search button click
 searchButton.addEventListener("click", (event) => {
     event.preventDefault(); // prevents default form submission
+    toggleNotification("off");
     const searchInput = document.getElementById("search-txt");
     const searchString = searchInput.value;
     if (searchString.trim().length === 0) { // if input is empty string
